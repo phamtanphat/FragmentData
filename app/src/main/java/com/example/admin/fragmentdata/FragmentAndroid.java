eac17093201bc0483f31a81b89d4611ba243b374
Phạm Tấn Phát <phatdroid94@gmail.com>
2018-09-06 18:55:50 +0700

    "Fragment view"

--- app/src/main/java/com/example/admin/fragmentdata/FragmentAndroid.java
+++ app/src/main/java/com/example/admin/fragmentdata/FragmentAndroid.java
@@ -1,4 +1,20 @@
 package com.example.admin.fragmentdata;
 
-public class FragmentAndroid {
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FragmentAndroid extends Fragment {
+
+    View view;
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_android,container,false);
+        return view;
+    }
 }
